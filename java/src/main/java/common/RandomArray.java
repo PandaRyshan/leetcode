10dc1b34c398354254dce19b25b625f7dd25a7c5
Xiaohong Hu <xiaohong@duckduck.io>
2022-11-15 21:12:46 +0800

    update common

---
+++ java/src/main/java/common/RandomArray.java
@@ -0,0 +1,11 @@
+package common;
+
+public class RandomArray {
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i ++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+}
